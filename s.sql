a4d9c3a182ef630f6d1aaf92046ec578b736671a
kianjohn01 <158255663+kianjohn01@users.noreply.github.com>
2026-03-24 00:40:43 +0800

    Create s.sql

---
+++ s.sql
@@ -0,0 +1,7 @@
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    isbn VARCHAR(50) NOT NULL,
+    file_format VARCHAR(50) NULL
+);
